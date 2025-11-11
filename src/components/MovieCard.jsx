d8b48d8eef0484a65fe6c74d6d078952e8ae7486
Noe <adegboyenoah@gmail.com>
2025-11-11 18:42:26 +0100

    little changes in the small and medium screens fav button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ function MovieCard({ movie }) {
             <div className="movie-card bg-movie-card group flex justify-between flex-col w-60 h-auto md:w-47 lg:w-60 h-auto xl:w-61 h-auto 2xl:w-69">
                 <div className="movie-poster relative">
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                    <div className="movie-overlay bg-movie-card-info border border-white rounded-full w-4 h-4 m-2 text-center absolute top-0 right-0 hidden group-hover:block p-4">
+                    <div className="movie-overlay bg-movie-card-info border border-white rounded-full w-4 h-4 m-2 text-center absolute top-0 right-0 sm:block md:block lg:hidden group-hover:block xl:hidden group-hover:block 2xl:hidden group-hover:block p-4">
                         <button className={`favourite-btn w-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${favorite ? 'text-red-700' : 'text-white'}`} onClick={ onFavouriteClick }>
                             ❤︎
                         </button>
