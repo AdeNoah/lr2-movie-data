515e3d562557595e533d008d30bba6ed159243bf
Noe <adegboyenoah@gmail.com>
2025-11-11 18:59:06 +0100

    quick fix to the favorites page background

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,7 +6,7 @@ function Favorites() {
 
   if (favorites) {
     return (
-      <div className="bg-background-color h-full flex flex-col pb-12 items-center">
+      <div className="bg-background-color h-screen flex flex-col pb-12 items-center">
         <h2 className="text-2xl font-bold my-4 text-white">Your Favorites</h2>
         <div className="movies-grid justify-items w-full grid gap-4 mx-auto place-items-center grid-cols-1 /*responsive*/ md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full 2xl:grid-cols-5 w-full">
           {favorites.map((movie) => (
@@ -17,20 +17,20 @@ function Favorites() {
     );
   }
 
-  return (
-    <>
-      <div className="favorite-empty bg-background-color h-screen text-white pt-24 px-12">
-        <div className="vvorite-card bg-empty-favorite-card text-center border-none rounded-lg p-12">
-          <h2 className="text-2xl text-red-500 font-bold mb-4">
-            No Favorite Movies Yet
-          </h2>
-          <p>
-            Start adding movies to your favorites and they will appear here.
-          </p>
-        </div>
-      </div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="favorite-empty bg-background-color h-screen text-white pt-24 px-12">
+  //       <div className="vvorite-card bg-empty-favorite-card text-center border-none rounded-lg p-12">
+  //         <h2 className="text-2xl text-red-500 font-bold mb-4">
+  //           No Favorite Movies Yet
+  //         </h2>
+  //         <p>
+  //           Start adding movies to your favorites and they will appear here.
+  //         </p>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 
